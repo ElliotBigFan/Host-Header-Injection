bc3c88ac0787e0d3f5ba3e3ca1ec8e1ec1d253ef
ElliotBigFan <lengocthuanti21@gmail.com>
2025-04-23 19:06:36 -0400

    revised

--- web-app/reset-password.php
+++ web-app/reset-password.php
@@ -4,4 +4,5 @@
     }
     $token = $_GET['token'];
     echo "token haven't been leaked: $token and it is not a flag. Everything is ok.";
+    echo "Click here to see your inbox: <a href='http://localhost:8081'>Inbox</a>";
 ?>
