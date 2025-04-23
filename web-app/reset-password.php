72225fac7701df6a61ffe837fbe7733624430f70
ElliotBigFan <lengocthuanti21@gmail.com>
2025-04-23 19:13:08 -0400

    revised

--- web-app/reset-password.php
+++ web-app/reset-password.php
@@ -3,6 +3,6 @@
         die('There is no token');
     }
     $token = $_GET['token'];
-    echo "token haven't been leaked: $token and it is not a flag. Everything is ok.";
+    echo "Token haven't been leaked: $token and it is not a flag. Everything is ok." . "<br>";
     echo "Click here to see your inbox: <a href='http://localhost:8081'>Inbox</a>";
 ?>
